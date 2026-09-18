2c2d30ddf78ecb5a84d155a3e94c8d125dfdf6c5
thomassombrun <tomasombrun@gmail.com>
2026-09-18 14:34:34 +0200

    wasted.go

---
+++ src/library/wasted.go
@@ -0,0 +1,12 @@
+package library
+
+import "fmt"
+
+func (c *Character) isDead() bool {
+	if c.CurrentHP <= 0 {
+		c.CurrentHP = c.MaxHP / 2
+		fmt.Printf("%s est mort... mais ressuscite avec %d PV !\n", c.Name, c.CurrentHP)
+		return true
+	}
+	return false
+}
