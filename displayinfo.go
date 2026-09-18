d2a1eb907e373dce49ab3e417819498c9e0eb683
NewHunter05 <nath.beaudri@gmail.com>
2026-09-18 12:14:35 +0200

    displayinfo.go

--- displayinfo.go
+++ displayinfo.go
@@ -2,10 +2,19 @@ package main
 
 import "fmt"
 
-func displayinfo(c Character) {
+func DisplayInfo(c Character) {
 	fmt.Printf("Name: %s\n", c.Name)
 	fmt.Printf("Class: %s\n", c.Class)
 	fmt.Printf("Level: %d\n", c.Level)
 	fmt.Printf("MaxHP: %d\n", c.MaxHP)
 	fmt.Printf("CurrentHP: %d\n", c.CurrentHP)
+}
+
+func main() {
+	c1.DisplayInfo()
+	fmt.Println("---")
+	c2.DisplayInfo()
+	fmt.Println("---")
+	c3.DisplayInfo()
+	fmt.Println("---")
 }
