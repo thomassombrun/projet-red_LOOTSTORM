d0b35a05926a86c5e7695aeec215ddd52051f76d
thomassombrun <tomasombrun@gmail.com>
2026-09-18 14:49:30 +0200

    character.go

--- src/library/character.go
+++ src/library/character.go
@@ -11,23 +11,27 @@ type Skill struct {
 }
 
 type Character struct {
-	Name      string
-	Class     string
-	Level     int
-	MaxHP     int
-	CurrentHP int
-	Inventory []Item
-	Skill     []Skill
+	Name        string
+	Class       string
+	Level       int
+	MaxHP       int
+	CurrentHP   int
+	Inventory   []Item
+	Skill       []Skill
+	PoisonTurns int
+	Gold        int
 }
 
 func InitCharacter(name string, class string, maxHP int) Character {
 	return Character{
-		Name:      name,
-		Class:     class,
-		Level:     1,
-		MaxHP:     maxHP,
-		CurrentHP: maxHP / 2,
-		Inventory: []Item{{Name: "Potion", Quantity: 3}},
-		Skill:     []Skill{{Name: "Coup de poing", Damage: 5}},
+		Name:        name,
+		Class:       class,
+		Level:       1,
+		MaxHP:       maxHP,
+		CurrentHP:   maxHP / 2,
+		Inventory:   []Item{{Name: "Potion", Quantity: 3}},
+		Skill:       []Skill{{Name: "Coup de poing", Damage: 5}},
+		PoisonTurns: 0,
+		Gold:        100,
 	}
 }
