0069a49f68fe9448098c92fb1d7c978666711681
thomassombrun <tomasombrun@gmail.com>
2026-09-18 12:04:00 +0200

    Update main.go

--- src/main.go
+++ src/main.go
@@ -1,1 +1,11 @@
 package main
+
+func main() {
+	c1 := initCharacter("Himiko Toga", "Assassin", 100)
+	c2 := initCharacter("Link", "Chevalier", 120)
+	c3 := initCharacter("Patrick Bouldefeu", "Mage", 80)
+
+	_ = c1
+	_ = c2
+	_ = c3
+}
