96c40d14e3526358c6143b4a92d2545e864161c1
thomassombrun <tomasombrun@gmail.com>
2026-09-18 11:55:20 +0200

    Update character.go

--- character.go
+++ character.go
@@ -1,1 +1,26 @@
 package main
+
+type Item struct {
+	Name     string
+	Quantity int
+}
+
+type Character struct {
+	Name      string
+	Class     string
+	Level     int
+	MaxHP     int
+	CurrentHP int
+	Inventory []Item
+}
+
+func initCharacter(name string, class string, maxHP int) Character {
+	return Character{
+		Name:      name,
+		Class:     class,
+		Level:     1,
+		MaxHP:     maxHP,
+		CurrentHP: maxHP / 2,
+		Inventory: []Item{{Name: "Potion", Quantity: 3}},
+	}
+}
