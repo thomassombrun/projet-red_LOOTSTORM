dd4f578978d93da4efba7bcae8137197a1ec8ff5
thomassombrun <tomasombrun@gmail.com>
2026-09-21 12:16:27 +0200

    maing.go

--- src/main.go
+++ src/main.go
@@ -5,7 +5,7 @@ import (
 	"projet/src/library"
 )
 
-func Main() {
+func main() {
 	c1 := library.InitCharacter("Himiko Toga", "Assassin", 100)
 	c2 := library.InitCharacter("Link", "Chevalier", 120)
 	c3 := library.InitCharacter("Patrick Bouldefeu", "Mage", 80)
